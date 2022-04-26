ed5107b2d8ee363b92bc77af38c7fd1e15d95cf3
alsrlqor <skyup9393@gmail.com>
2022-04-26 13:05:30 +0900

    Add Union

--- union-interface/typedefs-resolvers/givens.js
+++ union-interface/typedefs-resolvers/givens.js
@@ -1,11 +1,14 @@
 const { gql } = require('apollo-server')
 const dbWorks = require('../dbWorks.js')
 
+// Equipment 또는 Supply를 받을 수 있는 Union 선언
 const typeDefs = gql`
     union Given = Equipment | Supply
 `
+
 const resolvers = {
     Query: {
+        // equipements와 supplies를 둘 다 넣어서 반환하는 givens query 추가
         givens: (parent, args) => {
             return [
                 ...dbWorks.getEquipments(args),
@@ -14,6 +17,7 @@ const resolvers = {
         }
     },
     Given: {
+        // 각 항목 구분 역할: used_by면 __typename으로 Equipment, team이면 Supply
         __resolveType(given, context, info) {
             if (given.used_by) {
                 return 'Equipment'
@@ -25,6 +29,7 @@ const resolvers = {
         }
     }
 }
+
 module.exports = {
     typeDefs: typeDefs,
     resolvers: resolvers
