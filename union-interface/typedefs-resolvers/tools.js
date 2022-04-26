c0984fa8a5f837bb45338421f90ce7cb6da839bc
alsrlqor <skyup9393@gmail.com>
2022-04-26 12:31:09 +0900

    Union & Interface 사용

---
+++ union-interface/typedefs-resolvers/tools.js
@@ -0,0 +1,26 @@
+const { gql } = require('apollo-server')
+
+// 인터페이스 지정
+const typeDefs = gql`
+    interface Tool {
+        id: ID!
+        used_by: Role!
+    }
+`
+const resolvers = {
+    Tool: {
+        __resolveType(tool, context, info) {
+            if (tool.developed_by) {
+                return 'Software'
+            }
+            if (tool.new_or_used) {
+                return 'Equipment'
+            }
+            return null
+        }
+    }
+}
+module.exports = {
+    typeDefs: typeDefs,
+    resolvers: resolvers
+}
