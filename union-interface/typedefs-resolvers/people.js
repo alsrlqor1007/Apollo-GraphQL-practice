3b0593a719acab3da531f035504c1ecfaecd40d1
alsrlqor <skyup9393@gmail.com>
2022-04-29 21:12:41 +0900

    Add Filter and Pagination

--- union-interface/typedefs-resolvers/people.js
+++ union-interface/typedefs-resolvers/people.js
@@ -16,11 +16,28 @@ const typeDefs = gql`
         tools: [Tool]
         givens: [Given]
     }
+    input PostPersonInput {
+        first_name: String!
+        last_name: String!
+        sex: Sex!
+        blood_type: BloodType!
+        serve_years: Int!
+        role: Role!
+        team: ID!
+        from: String!
+    }
 `
 
 const resolvers = {
     Query: {
         people: (parent, args) => dbWorks.getPeople(args),
+        // 받아올 정보 필터링
+        peopleFiltered: (parent, args) => dbWorks.getPeople(args),
+        // 리스트 페이지네이션 구현 (필터링도 동시에 가능)
+        peoplePaginated: (parent, args) => dbWorks.getPeople(args),
+    },
+    Mutation: {
+        postPerson: (parent, args) => dbWorks.postPerson(args),
     }
 }
 
